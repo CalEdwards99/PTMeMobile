d8eaa87d32a1aaf59556dd4fa809c483ced7af94
(s) Callum Edwards 1 <CMDEdwards99@gmail.com>
2025-03-15 10:20:00 +0000

    Added expand collapse functionality to training page

--- src/pages/Train.jsx
+++ src/pages/Train.jsx
@@ -1,76 +1,104 @@
-import React from 'react';
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import { Text, TouchableOpacity } from 'react-native'
 import { DataTable } from 'react-native-paper';
+import Collapsible from 'react-native-collapsible';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from '../styles/style.jsx';
 
-const MyComponent = () => (
-    <>
-        <Text style={[styles.textCenter, { fontSize: 18, marginTop: 7 }]}>Push Day 1</Text>
-        <DataTable>
-            <DataTable.Header>
-                <DataTable.Title>Exercise</DataTable.Title>
-                <DataTable.Title numeric>Weight (KG)</DataTable.Title>
-                <DataTable.Title numeric>Reps</DataTable.Title>
-            </DataTable.Header>
-        </DataTable>
+export default function Train() {
+    const [chevron, setChevron] = useState("chevron-down")
+    const [rowOpen, setRowOpen] = useState(false);
+    const [collapsed, setCollapsed] = useState(true);
 
-        <DataTable>
-            <DataTable.Header>
-                <DataTable.Cell>Barbell Benchpress</DataTable.Cell>
-                <DataTable.Title numeric><Icon name="chevron-up" size={15} color={"black"}/></DataTable.Title>
-            </DataTable.Header>
+    function OpenRowHandler() {
+        {
+            rowOpen ? (
+                setChevron('chevron-up'),
+                setRowOpen(false),
+                setCollapsed(false)
+            ) : (
+                setChevron('chevron-down'),
+                setRowOpen(true),
+                setCollapsed(true)
+            )
+        }
+    }
 
-            <DataTable.Row>
-                <DataTable.Cell>Set 1</DataTable.Cell>
-                <DataTable.Cell numeric>100</DataTable.Cell>
-                <DataTable.Cell numeric>6</DataTable.Cell>
-            </DataTable.Row>
+    return (
+        <>
+            <Text style={[styles.textCenter, { fontSize: 18, marginTop: 7 }]}>Push Day 1</Text>
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Title>Exercise</DataTable.Title>
+                    <DataTable.Title numeric>Weight (KG)</DataTable.Title>
+                    <DataTable.Title numeric>Reps</DataTable.Title>
+                </DataTable.Header>
+            </DataTable>
 
-            <DataTable.Row>
-                <DataTable.Cell>Set 2</DataTable.Cell>
-                <DataTable.Cell numeric>35</DataTable.Cell>
-                <DataTable.Cell numeric>8</DataTable.Cell>
-            </DataTable.Row>
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Cell>Barbell Benchpress</DataTable.Cell>
+                    <DataTable.Title numeric>
+                        <TouchableOpacity onPress={OpenRowHandler}>
+                            <Icon name={chevron} size={15} color={"black"} />
+                        </TouchableOpacity>
+                    </DataTable.Title>
+                </DataTable.Header>
 
-            <DataTable.Row>
-                <DataTable.Cell>Set 3</DataTable.Cell>
-                <DataTable.Cell numeric>100</DataTable.Cell>
-                <DataTable.Cell numeric>6</DataTable.Cell>
-            </DataTable.Row>
+                <Collapsible collapsed={collapsed} duration={475} >
+                    <DataTable.Row>
+                        <DataTable.Cell>Set 1</DataTable.Cell>
+                        <DataTable.Cell numeric>100</DataTable.Cell>
+                        <DataTable.Cell numeric>6</DataTable.Cell>
+                    </DataTable.Row>
 
-            <DataTable.Row>
-                <DataTable.Cell>Set 4</DataTable.Cell>
-                <DataTable.Cell numeric>100</DataTable.Cell>
-                <DataTable.Cell numeric>6</DataTable.Cell>
-            </DataTable.Row>
-        </DataTable>
-        
-        <DataTable>
-            <DataTable.Header>
-                <DataTable.Cell>Dumbell Shoulderpress</DataTable.Cell>
-                <DataTable.Title numeric><Icon name="chevron-down" size={15} color={"black"}/></DataTable.Title>
-            </DataTable.Header>
-        </DataTable>
+                    <DataTable.Row>
+                        <DataTable.Cell>Set 2</DataTable.Cell>
+                        <DataTable.Cell numeric>35</DataTable.Cell>
+                        <DataTable.Cell numeric>8</DataTable.Cell>
+                    </DataTable.Row>
 
-        <DataTable>
-            <DataTable.Header>
-                <DataTable.Cell>Tricep Pushdown</DataTable.Cell>
-                <DataTable.Title numeric><Icon name="chevron-down" size={15} color={"black"}/></DataTable.Title>
-            </DataTable.Header>
+                    <DataTable.Row>
+                        <DataTable.Cell>Set 3</DataTable.Cell>
+                        <DataTable.Cell numeric>100</DataTable.Cell>
+                        <DataTable.Cell numeric>6</DataTable.Cell>
+                    </DataTable.Row>
 
-            <DataTable.Pagination
-                page={1}
-                numberOfPages={3}
-                onPageChange={page => {
-                    console.log(page);
-                }}
-                label="1-2 of 6"
-            />
-        </DataTable>
+                    <DataTable.Row>
+                        <DataTable.Cell>Set 4</DataTable.Cell>
+                        <DataTable.Cell numeric>100</DataTable.Cell>
+                        <DataTable.Cell numeric>6</DataTable.Cell>
+                    </DataTable.Row>
+                </Collapsible>
+            </DataTable>
 
-    </>
-);
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Cell>Dumbell Shoulderpress</DataTable.Cell>
 
-export default MyComponent;
+                    <DataTable.Title numeric>
+                        <Icon name="chevron-down" size={15} color={"black"} />
+                    </DataTable.Title>
+                </DataTable.Header>
+            </DataTable>
+
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Cell>Tricep Pushdown</DataTable.Cell>
+                    <DataTable.Title numeric><Icon name="chevron-down" size={15} color={"black"} /></DataTable.Title>
+                </DataTable.Header>
+
+                <DataTable.Pagination
+                    page={1}
+                    numberOfPages={3}
+                    onPageChange={page => {
+                        console.log(page);
+                    }}
+                    label="1-2 of 6"
+                />
+            </DataTable>
+
+        </>
+    )
+};
